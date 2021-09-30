c48ead0c6bc3d058e22e31f9c4db8cad90517eb9
krasio12356 <krasio@izborchoice.com>
2021-09-30 15:28:07 +0300

    Experiment with components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home';
+import Register from './components/Register';
+function App() 
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul className='navul'>
+            <li className='navli'>
+              <Link to="/" className='navlink'>Home</Link>
+            </li>
+            <li className='navli'>
+              <Link to="/register" className='navlink'>Register</Link>
+            </li>
+            <li className='navli'>
+              <Link to="/users" className='navlink'>Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/register" component={Register}/>
+            
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
---
+++ src/components/Register.js
@@ -0,0 +1,95 @@
+import './Register.module.css';
+import React from "react";
+
+class Register extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = 
+        {
+            name: '',
+            password: '',
+            confirm: '',
+            errors: []
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(e)
+    {
+        if (e.target.name === 'name')
+        {
+            this.setState({name: e.target.value});
+        }
+        else if (e.target.name === 'password')
+        {
+            this.setState({password: e.target.value});
+        }
+        else if (e.target.name === 'confirm')
+        {
+            this.setState({confirm: e.target.value});
+        }
+    }
+    async handleSubmit(e)
+    {
+        e.preventDefault();
+        let err = '';
+        if (this.state.name == '')
+        {
+            err += '<br>Name is compulsory.';
+            this.setState({errors: err});
+        }
+        if (this.state.password.length < 3)
+        {
+            err += '<br>Password must be at least 3 symbols long.';
+            this.setState({errors: err});
+        }
+        if (this.state.password != this.state.confirm)
+        {
+            err += '<br>Password and confirm password must be the same.';
+            this.setState({errors: err});
+        }
+        if (this.state.errors == '')
+        {
+            try
+            {
+                let obj =
+                {
+                    name: this.state.name,
+                    password: this.state.password
+                };
+                let response = await fetch('http://localhost:5000/register',
+                {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+                    body: JSON.stringify(obj)
+                });
+                //let data = await response.json();
+                
+                //this.props.history.push('/');
+            }
+            catch(er)
+            {
+
+            }
+        }
+    }
+    render()
+    {
+        return (
+            <form className='regform' onSubmit={this.handleSubmit}>
+                <p><label>Name:</label></p>
+                <p><input className='reginput' type="text" name='name' value={this.state.value} onChange={this.handleChange} /></p>        
+                <p><label>Password:</label></p>
+                <p><input className='reginput' type="password" name='password' value={this.state.value} onChange={this.handleChange} /> </p>       
+                <p><label>Confirm password:</label></p>
+                <p><input className='reginput' type="password" name='confirm' value={this.state.value} onChange={this.handleChange} /> </p>       
+                <p><input className='reginput' type="submit" value="Submit" /></p>
+                <p className='errorText' dangerouslySetInnerHTML={{__html: this.state.errors}}></p>
+            </form>
+        );
+    }
+}
+
+export default Register
